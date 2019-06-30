4b5e732cf95254860acfa821c61a735a51b2cae9
Tanvir Islam Streame <streametanvir@gmail.com>
2019-06-30 16:03:23 +0600

    Route added and saved form in local storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //Components import
 import EmployeeRegister from "./components/EmployeeRegister"
 
 function App() {
   return (
-    <div className="App">
-       <EmployeeRegister />
+    <div >
+        <Router>
+          <Route exact path="/" component={EmployeeRegister} />
+      </Router>
     </div>
   );
 }
